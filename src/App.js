3414079215800657e0518eeb179d5121fb3f85ab
ANERI SHAH <avs8738@nyu.edu>
2023-12-03 14:45:26 -0500

    first push with basic features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const AllowedKeysInput = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (event) => {
+    if (event.key !== 'Enter' && event.key !== 'Backspace' && (event.key !== 'a' && event.key !== 's' && event.key !== 'd' && event.key !== 'f')) {
+      event.preventDefault();
+    }
+    //arrow keys
+     if(event.key === 'h'){
+      window.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowLeft' }));
+    } else if(event.key === 'j'){
+      window.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp' }));
+    } else if(event.key === 'k'){
+      window.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
+    } else if(event.key === 'l'){
+      window.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowRight'}));
+    }
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="input-container">
+      <input type="text" value={inputValue} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <p>You can only use the keys 'a', 's', 'd', 'f'</p>
     </div>
   );
-}
+};
 
-export default App;
+export default AllowedKeysInput;
